5dd2988652830cc5e05262b7fdb7949b52b59365
DavidUdala <david_udala@hotmail.com>
2024-08-22 13:13:23 -0300

    projeto inicial

---
+++ PedidosProdutos.API/Controllers/ProdutoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PedidosProdutos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProdutoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Listar() { 
+            return Ok();    
+        }
+    }
+}
